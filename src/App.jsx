3ef9c4e22de21850edd0bdb66d3e1ee96b135990
Ale <alem7486@gmail.com>
2025-05-31 21:18:03 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,4 @@ import Tarjeta from './components/Tarjeta'
 
 
 function App(){
-
-  let productos = ['tele', 'cama', 'sofá', 'escritorio']
-  const promos = [
-    {
-      id: 1,
-      titulo: 'Placa de video',
-      descripcion: 'Aprovechá un 20% de descuento',
-      imageURL: 'https://fullh4rd.com.ar/img/productos/3/video-geforce-rtx-4060-8gb-gigabyte-gaming-oc-0.jpg',
-      enlace: '/descuento1'
-    },
-      
-    {
-      id: 2,
-      titulo: 'Placa de audio',
-      descripcion: 'Aprovechá un 20% de descuento',
-      imageURL: 'https://fullh4rd.com.ar/img/productos/3/video-geforce-rtx-4060-8gb-gigabyte-gaming-oc-0.jpg',
-      enlace: '/descuento2'
-
-    }
-  ]
-
-  return (
-    <>
-    {promos.map((promos) => <Tarjeta/>
-    )}
-
-    </>
-
-export default App
+}
